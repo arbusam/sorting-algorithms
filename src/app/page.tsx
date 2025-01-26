a482fa5965bba179dc2f4ee7828b64c9ce3952ed
arbusam <arhan.busam@gmail.com>
2025-01-26 23:38:51 +1100

    Added links to footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -611,7 +611,53 @@ export default function Home() {
         </div>
       </div>
       <footer className="bg-gray-400 dark:bg-gray-600 p-4">
+        <div className="flex justify-between items-center">
+          <div>
         © 2025 Arhan Busam. Please note the speed of each graph is not perfectly to scale to the other graphs. Clicking each sorting algorithm heading will open an external site with more information. I do not have any control over the content of the external site.
+          </div>
+          <div className="flex space-x-5 rtl:space-x-reverse">
+        <a
+          href="https://github.com/arbusam/sorting-algorithms/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
+        >
+          <svg
+            className="h-4 w-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+          fillRule="evenodd"
+          d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+          clipRule="evenodd"
+            />
+          </svg>
+          <span className="sr-only">GitHub account</span>
+        </a>
+        <a
+          href="https://bsky.app/profile/arhan.tech"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
+        >
+          <svg
+            className="h-4 w-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 568 501"
+          >
+            <path
+          fill="currentColor"
+          d="M123.121 33.664C188.241 82.553 258.281 181.68 284 234.873c25.719-53.192 95.759-152.32 160.879-201.21C491.866-1.611 568-28.906 568 57.947c0 17.346-9.945 145.713-15.778 166.555-20.275 72.453-94.155 90.933-159.875 79.748C507.222 323.8 536.444 388.56 473.333 453.32c-119.86 122.992-172.272-30.859-185.702-70.281-2.462-7.227-3.614-10.608-3.631-7.733-.017-2.875-1.169.506-3.631 7.733-13.43 39.422-65.842 193.273-185.702 70.281-63.111-64.76-33.89-129.52 80.986-149.071-65.72 11.185-139.6-7.295-159.875-79.748C9.945 203.659 0 75.291 0 57.946 0-28.906 76.135-1.612 123.121 33.664Z"
+            />
+          </svg>
+        </a>
+        </div>
+        </div>
       </footer>
     </div>
   );
